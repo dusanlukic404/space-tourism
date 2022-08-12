b539d59b2793d75c382ce32333a24cff477a86d3
Dusan Lukic <dusanlukic404@gmail.com>
2022-08-12 17:24:45 +0200

    added js for crew

--- public/js/crew.js
+++ public/js/crew.js
@@ -1,1 +1,38 @@
 "use strict";
+
+fetch("../data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const crewData = data.crew;
+    const crewLinks = document.querySelectorAll(".crew-link");
+    const crewLinksContainer = document.querySelector(".crew-links");
+    let selectedCrew = "";
+
+    crewLinksContainer.addEventListener("click", function (e) {
+      e.preventDefault();
+      if (!e.target.classList.contains("crew-link")) return;
+
+      crewLinks.forEach((link) => link.classList.remove("crew-link--active"));
+      e.target.classList.add("crew-link--active");
+
+      selectedCrew = e.target.dataset.crewname;
+
+      const [dataForCrew] = crewData.filter(
+        (data) => selectedCrew === data.name
+      );
+
+      changeCrew(dataForCrew);
+    });
+  });
+
+function changeCrew(data) {
+  const crewProfession = document.querySelector(".crew-text__profession");
+  const crewName = document.querySelector(".crew-text__name");
+  const crewBio = document.querySelector(".crew-text__bio");
+  const crewImg = document.querySelector(".crew-img");
+
+  crewProfession.textContent = data.role;
+  crewName.textContent = data.name;
+  crewBio.textContent = data.bio;
+  crewImg.src = data.images.png;
+}
