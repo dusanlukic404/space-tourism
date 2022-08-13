68b178ecd6c6c86768de1c259479f64f80cf73c8
Dusan Lukic <dusanlukic404@gmail.com>
2022-08-14 00:30:53 +0200

    refactored code in main script

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,27 @@
 const btnMenu = document.querySelector(".btn-menu");
 const primaryNav = document.querySelector(".primary-navigation");
 
+if (window.innerWidth < 620) {
+  primaryNav.setAttribute("aria-expanded", false);
+} else {
+  primaryNav.setAttribute("aria-expanded", true);
+}
+
 btnMenu.addEventListener("click", function () {
   btnMenu.classList.toggle("btn-menu--active");
   if (btnMenu.classList.contains("btn-menu--active")) {
-    primaryNav.style.transform = "translateX(0)";
+    openNav();
   } else {
-    primaryNav.style.transform = "translateX(100%)";
+    closeNav();
   }
 });
+
+function openNav() {
+  primaryNav.style.transform = "translateX(0)";
+  primaryNav.setAttribute("aria-expanded", true);
+}
+
+function closeNav() {
+  primaryNav.style.transform = "translateX(100%)";
+  primaryNav.setAttribute("aria-expanded", false);
+}
